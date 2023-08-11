5bfcf46d6d00a144626417e859b082ea719a4df2
Dennys Mosquera <dennys.mosquera@kinesso.com>
2023-08-11 16:32:57 -0500

    trying to improve the selectors

--- cypress/page/login.page.ts
+++ cypress/page/login.page.ts
@@ -1,26 +1,25 @@
 class LoginPage {
-    private loginURL: string;
-    private userNameField: string;
-    private passwordField: string;
-    private loginButton: string;
-  
-    constructor() {
-      this.loginURL = "http://saucedemo.com/";
-      this.userNameField = ".login-box > form > div > input#user-name";
-      this.passwordField = ".login-box > form > div > input#password";
-      this.loginButton = "input[type='submit']";
-    }
-  
-    public visitLoginPage(): void {
-      cy.visit(this.loginURL);
-    }
-  
-    public signIn(): void {
-      cy.get(this.userNameField).type("standard_user");
-      cy.get(this.passwordField).type("secret_sauce");
-      cy.get(this.loginButton).click();
-    }
+  private loginURL: string;
+  private userNameField: string;
+  private passwordField: string;
+  private loginButton: string;
+
+  constructor() {
+    this.loginURL = "http://saucedemo.com/";
+    this.userNameField = "[data-test='username']";
+    this.passwordField = "[data-test='password']";
+    this.loginButton = "[data-test='login-button']";
   }
-  
-  export { LoginPage };
-  
+
+  public visitLoginPage(): void {
+    cy.visit(this.loginURL);
+  }
+
+  public signIn(): void {
+    cy.get(this.userNameField).type("standard_user");
+    cy.get(this.passwordField).type("secret_sauce");
+    cy.get(this.loginButton).click();
+  }
+}
+
+export { LoginPage };
